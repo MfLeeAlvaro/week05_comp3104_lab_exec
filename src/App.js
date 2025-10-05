3a41f71573d831f1a274f07f01134e2967060cbd
MfLeeAlvaro <101421575@georgebrown.ca>
2025-10-05 13:52:01 -0400

    final updates to package.json and App.js before merging into main

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>COMP3104 - Lab 05</h1>
+        <h2>Student: Jon Adrian Lee Alvaro</h2>
+        <h3>ID: 101421575</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Single Page Application is running successfully 🚀
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Branch: <code>dep-inclusions</code>
+        </p>
       </header>
     </div>
   );
